63fa557a6a0f050c65039064af5388625781668e
Andrey <andreynrsa@gmail.com>
2020-09-15 14:43:33 -0300

    first commit

---
+++ api/src/main/java/com/americas/challenge/api/Application.java
@@ -0,0 +1,14 @@
+package com.americas.challenge.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(final String[] args) {
+		System.out.println("START PROJECT SUCESS");
+		SpringApplication.run(Application.class, args);
+	}
+
+}
